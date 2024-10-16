82e7becde91e043a4ec46b4d0dadce6b196214f1
donatofranco <francoedonato@gmail.com>
2024-10-16 20:46:57 -0300

    Remove META-INF

--- src/main/java/noticracia/PajaritoInformationSource.java
+++ src/main/java/noticracia/PajaritoInformationSource.java
@@ -5,7 +5,6 @@ import noticracia.mappers.InformationMapper;
 import noticracia.services.ServerService;
 import noticracia.services.UpdateHandlerService;
 
-import java.util.HashMap;
 import java.util.Map;
 import java.util.stream.Collectors;
 
@@ -37,20 +36,19 @@ public class PajaritoInformationSource extends InformationSource {
         String[] words = this.searchCriteria.toLowerCase().split(" ");
 
         return new InformationMapper().mapInformation((String) response).entrySet().stream()
-                //Le quito las informaciones que no traten de mi searchCriteria
-                .filter(e -> e.getValue().toLowerCase().contains(this.searchCriteria.toLowerCase()))
-                .collect(Collectors.toMap(
-                        //Le quito las apariciones del searchCriteria
-                        Map.Entry::getKey,
-                        e -> {
-                            String modifiedValue = e.getValue();
-                            for (String word : words) {
-                                modifiedValue = modifiedValue.replace(word, "");
-                            }
-                            return modifiedValue;
-                        }
-                ));
+            //Le quito las informaciones que no traten de mi searchCriteria
+            .filter(e -> e.getValue().toLowerCase().contains(this.searchCriteria.toLowerCase()))
+            .collect(Collectors.toMap(
+                //Le quito las apariciones del searchCriteria
+                Map.Entry::getKey,
+                e -> {
+                    String modifiedValue = e.getValue();
+                    for (String word : words) {
+                        modifiedValue = modifiedValue.replace(word, "");
+                    }
+                    return modifiedValue;
+                }
+            ));
     }
-
 
 }
